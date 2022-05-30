b8614d2cd0dadb9a2c767620385ed5a47ee11637
Masoudi Parrot <masoudi@localhost.localdomain>
2022-05-30 19:04:45 -0400

    Create table call animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED DEFAULT AS IDENTITY,
+    name VARCHAR(50),
+    date_of_birth DATE,
+    escape_attempts CHAR(15),
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
+    PRIMARY KEY(id)
 );

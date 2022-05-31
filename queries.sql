d7ac5ea713bde557e68002b0413f70489a95057b
Masoudi Parrot <masoudi@localhost.localdomain>
2022-05-31 16:58:14 -0400

    Add a column species to animals table

--- queries.sql
+++ queries.sql
@@ -32,3 +32,32 @@ WHERE name != 'Gabumon';
 SELECT * 
 FROM animals
 WHERE weight_kg <= 10.4 AND weight_kg >= 17.3;
+
+/* Query and update animals table */
+
+SELECT COUNT(*) AS Total
+FROM animals;
+
+SELECT COUNT(*) as Not_escape
+FROM animals
+WHERE escape_attempts > 0;
+
+SELECT AVG(weight_kg) as Average
+FROM animals;
+
+SELECT neutered, SUM(escape_attempts)
+AS N_Escape  
+FROM animals
+GROUP BY neutered;
+
+SELECT species, 
+MAX(weight_kg) AS maximum, 
+MIN(weight_kg) AS minimum
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) as Average
+FROM animals
+WHERE date_of_birth BETWEEN '1990/12/31' AND '2000/12/31'
+GROUP BY species;
+
